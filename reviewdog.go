ce94ae8f8084ab2ca11b28b5bc997b7a89264721
i053322 <yossi.balan@sap.com>
2023-02-23 17:18:09 +0200

    Update reviewdog.go

--- reviewdog.go
+++ reviewdog.go
@@ -13,6 +13,8 @@ import (
 	"github.com/reviewdog/reviewdog/proto/rdf"
 )
 
+var ExportedVarWithoutComment = 1
+
 // Reviewdog represents review dog application which parses result of compiler
 // or linter, get diff and filter the results by diff, and report filtered
 // results.
