c6322cbe643d76b69cd57ef2e713bd1d26310210
i053322 <yossi.balan@sap.com>
2023-02-23 16:53:50 +0200

    Update reviewdog.go

--- reviewdog.go
+++ reviewdog.go
@@ -60,6 +60,8 @@ type DiffService interface {
 	Strip() int
 }
 
+var ExportedVarWithoutComment = 1 // TODO: revert
+
 func (w *Reviewdog) runFromResult(ctx context.Context, results []*rdf.Diagnostic,
 	filediffs []*diff.FileDiff, strip int, failOnError bool) error {
 	wd, err := os.Getwd()
